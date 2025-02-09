d1d5d17ba1693e0df09d5e1a47cfbc1dc06e233c
w-jamil <139834002+w-jamil@users.noreply.github.com>
2025-02-09 17:22:37 +0000

    pal

---
+++ pal/base.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+
+
+class BaseModel(object):
+    """This class implements a base model
+    """
+
+    def __init__(self, n):
+        self.n = n
+        self.w = np.matrix(np.ones(self.n))
+
+
+    def get_params(self):
+        """return current parameters
+        """
+        return {
+            'w': self.w
+            }
+    
+    def apply_updates(self, updates):
+        """apply new param updates to the current model
+        """
+        if 'w' in updates:
+            self.w = np.matrix(updates['w'])
+    
+    @property
+    def delta(self, x, y):
+        """calculate model updates
+        """
+        raise NotImplementedError
+
+    @property
+    def predict(self, x):
+        """make prediction
+        """
+        raise NotImplementedError
